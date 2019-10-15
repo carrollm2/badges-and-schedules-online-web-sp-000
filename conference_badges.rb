acdbbd26b85044067140e20c095922e38b9126fd
Michael Carroll <carrollm2@gmail.com>
2019-10-15 02:18:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,12 +4,10 @@ end
 
 
 def batch_badge_creator(speakers)
-  #room_assignments = assign_roooms(speakers)
 
   badge_messages = []
+
   speakers.each do |name|
-    #assigned_room = room_assignments[name]
-    #badge_messages.push("Hello, my name is #{name}! You'll be assinged room #{assigned_room}")
     badge_messages.push("Hello, my name is #{name}.")
   end
 
@@ -20,15 +18,15 @@ end
 
 def assign_rooms(speakers)
 
-  #assignment_dict = {}
+
   welcome_messages = []
 
   speakers.each_with_index do |name, index|
     welcome_messages.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
-    #assignment_dict[name] = index
   end
+  
   return welcome_messages
-  #return assignment_dict
+
 end
 
 
